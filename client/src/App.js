ef7b4f21b141142ef43f28d335a9313d1c7ea7f1
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-01 12:11:11 -0500

    Changed name

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from "./test/Home";
+import Home from "./pages/Home";
 function App(){
 
     return(
