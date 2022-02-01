d6dad3ff126da60a40500a56c7f8a80058cb8161
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-01 11:37:11 -0500

    Error in Home module

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from "./pages/Home";
+import Home from "./pages/Home.js";
 function App(){
 
     return(
