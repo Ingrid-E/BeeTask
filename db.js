0500c6d0785d9ae86c9f111cbbd32c7111515b26
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-01 11:57:20 -0500

    fixing

--- db.js
+++ db.js
@@ -11,8 +11,11 @@ const devConfig = {
 };
 
 const proConfig = {
-  connectionString: process.env.DATABASE_URL, //Viene de heroku
-};
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+}
 
 const client = new Pool(
   process.env.NODE_ENV === "production" ? proConfig : devConfig
