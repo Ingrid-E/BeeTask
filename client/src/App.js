177550508cbb371c6a246d9a3ca89f46492f1fa0
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-01 12:09:42 -0500

    make it work

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from "./pages/Home";
+import Home from "./test/Home";
 function App(){
 
     return(
