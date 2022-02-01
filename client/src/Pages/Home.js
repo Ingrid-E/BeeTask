f58357bd05a8806306fa8e9a368777eb5d84cd2d
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-01 11:21:03 -0500

    Subiendo a Heroku

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,6 +1,15 @@
 import React from "react";
-
+import {get} from '../hooks/Hooks'
 function Home(){
+    apiTEST();
+    async function apiTEST() {
+        try {
+          const response =await get(`/users/`)
+          console.log(response)
+        } catch (error) {
+          console.error("Not working");
+        }
+      }
     return (
         <div className="Home">
             <h1>HOLA GEYS</h1>
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const pool = require("../db");
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET users listing. Prueba de escritorio*/
+router.get("/", async (req, res) => {
+  try {
+    const response = await pool.query(`SELECT * FROM users`);
+    return res.status(200).json(response.rows);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
