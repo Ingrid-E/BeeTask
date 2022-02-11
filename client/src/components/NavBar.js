e4d766140c509c3d396ebc3043da7d574d50f5fb
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-10 22:50:47 -0500

    Navigation Bar and Home page in progress

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,31 @@
-import React from "react";
-
-function NavBar(){
-    return(
-        <div className="NavBar">
-            <h1>Hola</h1>
-        </div>
-    )
+import React, { useState } from "react";
+import { ReactComponent as Logo } from "../assets/Images/BeeTask.svg";
+import "./components.css";
+import "./hamburger.css";
+function NavBar() {
+  const [menu, setMenu] = useState(false);
+  return (
+    <div className="navbar">
+      <Logo className="navbar__logo"></Logo>
+      <ul className={menu ? "open" : ""}>
+        <li>INICIO</li>
+        <li>NOSOTROS</li>
+        <li>RECURSOS</li>
+        <li className="navbar__important">INICIAR SESIÓN</li>
+        <li className="navbar__button yellow-button">REGISTRARSE</li>
+      </ul>
+      <div
+        className={`navbar__hamburger ${menu ? "open" : ""}`}
+        onClick={() => {
+          setMenu(!menu);
+        }}
+        >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,25 @@
-import React, {useEffect} from "react";
-import NavBar from "../components/NavBar";
-import {get} from '../hooks/Hooks'
-
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react"
+import NavBar from "../components/NavBar"
+import './pages.css'
+import PeopleImages from '../assets/Images/People-Images.png'
 function Home(){
-    useEffect(() => {
-        apiTEST();
-      }, [])
 
-    async function apiTEST() {
-        try {
-          const response =await get(`test`)
-          console.log(response)
-        } catch (error) {
-          console.error("Not working");
-        }
-      }
     return (
-        <div className="Home">
-            <NavBar/>
+        <div className="home">
+          <NavBar/>
+          <div className="home__start">
+            <div className="home__start-text">
+            <h1>Organizate a tu manera</h1>
+            <p>Todas las herramientas que necesitas en un solo lugar. Comienza a utilizarlas ya!</p>
+            <button className="yellow-button">REGISTRARSE</button>
+            </div>
+            <div className="home__start-image">
+            <img src={PeopleImages}></img>
+            </div>
+          </div>
+          <div className="home__section2">
+          </div>
         </div>
     )
 }
