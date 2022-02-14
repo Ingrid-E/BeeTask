aa4a070b2ac8a93deb82d33233718dbf59341d0d
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-14 16:28:58 -0500

    Working on second section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,11 +14,24 @@ function Home(){
             <p>Todas las herramientas que necesitas en un solo lugar. Comienza a utilizarlas ya!</p>
             <button className="yellow-button">REGISTRARSE</button>
             </div>
-            <div className="home__start-image">
-            <img src={PeopleImages}></img>
-            </div>
           </div>
           <div className="home__section2">
+            <span className="hex hex-left"></span>
+            <div className="home__section2-text">
+            <div className="text-float">
+              <h1>Tareas</h1>
+              <p>Organiza tus tareas por materia y fecha. Puedes agregar la dificultad de cada una y priorizar cuales hacer primero.</p>
+            </div>
+            <div className="text-float">
+              <h1>Notas</h1>
+              <p>Agrega las notas de tus tareas, para tener en cuenta tu rendimiento en esa materia.</p>
+            </div>
+            <div className="text-float">
+              <h1>Calcula</h1>
+              <p>Calcula tu nota en la materia y cuanto necesitas para pasar!</p>
+            </div>
+          </div>
+          <span className="hex hex-right"></span>
           </div>
         </div>
     )
