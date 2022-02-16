1d9cb6c8f03b56b47ad703b9f1297db6074fea4a
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2022-02-16 14:34:43 -0500

    Inicio Seccion 3

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,44 +1,65 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from "react"
-import NavBar from "../components/NavBar"
-import './pages.css'
-import PeopleImages from '../assets/Images/People-Images.png'
-function Home(){
-
-    return (
-        <div className="home">
-          <NavBar/>
-          <div className="home__start">
-            <div className="home__start-text">
-            <h1>Organizate a tu manera</h1>
-            <p>Todas las herramientas que necesitas en un solo lugar. Comienza a utilizarlas ya!</p>
-            <button className="yellow-button">REGISTRARSE</button>
-            </div>
+import React from "react";
+import NavBar from "../components/NavBar";
+import BeeHive from '../assets/Images/BeeHive.png'
+import "./pages.css";
+function Home() {
+  return (
+    <div className="home">
+      <NavBar />
+      <div className="home__start">
+        <div className="home__start-text">
+          <h1>Organizate a tu manera</h1>
+          <p>
+            Todas las herramientas que necesitas en un solo lugar. Comienza a
+            utilizarlas ya!
+          </p>
+          <button className="yellow-button">REGISTRARSE</button>
+        </div>
+      </div>
+      <div className="home__section2">
+        <div className="home__section2-text">
+          <div className="text-float">
+            <h1>Tareas</h1>
+            <p>
+              Organiza tus tareas por materia y fecha. Puedes agregar la
+              dificultad de cada una y priorizar cuales hacer primero.
+            </p>
           </div>
-          <div className="home__section2">
-            <div className="home__section2-text">
-            <div className="text-float">
-              <h1>Tareas</h1>
-              <p>Organiza tus tareas por materia y fecha. Puedes agregar la dificultad de cada una y priorizar cuales hacer primero.</p>
-            </div>
-            <div className="text-float">
-              <h1>Notas</h1>
-              <p>Agrega las notas de tus tareas, para tener en cuenta tu rendimiento en esa materia.</p>
-            </div>
-            <div className="text-float">
-              <h1>Calcula</h1>
-              <p>Calcula tu nota en la materia y cuanto necesitas para pasar!</p>
-            </div>
+          <div className="text-float">
+            <h1>Notas</h1>
+            <p>
+              Agrega las notas de tus tareas, para tener en cuenta tu
+              rendimiento en esa materia.
+            </p>
           </div>
-          <div className="home__section2-background">
-            <div className="hex hex-left"></div>
-            <div className="hex hex-right"></div>
-            <div className="bee bee-top"></div>
-            <div className="bee bee-down"></div>
-            </div>
+          <div className="text-float">
+            <h1>Calcula</h1>
+            <p>Calcula tu nota en la materia y cuanto necesitas para pasar!</p>
           </div>
         </div>
-    )
+        <div className="home__section2-background">
+          <div className="hex hex-left"></div>
+          <div className="hex hex-right"></div>
+          <div className="bee bee-top"></div>
+          <div className="bee bee-down"></div>
+        </div>
+      </div>
+      <div className="home__info">
+        <div className="home__info-text">
+          <h1>Titulo</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique
+            non elementum in tempor, a dis. Quis nisl vel sodales praesent elit
+            amet. Tempor venenatis diam vulputate fames commodo. Ac id proin
+            molestie egestas elit at gravida vehicula sed.
+          </p>
+          <button className="yellow-button">Texto</button>
+        </div>
+        <img src={BeeHive}></img>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
