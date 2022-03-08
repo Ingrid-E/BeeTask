511b9d482bf2d5b4d45f30797d8e0082884f938d
jhan-ai-s13 <jhan.perez@correounivalle.edu.co>
2022-03-08 14:58:57 -0500

    fecha

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -8,7 +8,17 @@ function TaskForm(props){
     let [task, setTask] = useState({
         name: '',
         description: '',
+        datetime:'',
         grade: 0, 
+    });
+
+    const [dateTime, setDateTime] = useState({
+        day: 0,
+        month: 0,
+        year: 0,
+        hours: 0,
+        minutes: 0, 
+        am_pm: ''
     });
 
     useEffect(()=>{
@@ -30,14 +40,45 @@ function TaskForm(props){
           setTask({...task, [e.target.name]:e.target.value} )
       };
 
+      const handleDate = async(e) => {
+        
+        setDateTime({...dateTime, [e.target.name]:e.target.value});
+
+    };
+
+      const HoursConverter12FormatTo24Format = (hour12Format, aMinute, amOrPm) =>{
+        let hour24Format = 0;
+        if(amOrPm == 'AM'){
+            if(hour12Format == 12){
+                hour24Format = 0;
+            }else{
+                hour24Format = hour12Format;
+            }
+        }
+
+        if(amOrPm=== 'PM'){
+            if(hour12Format == 12){
+
+                hour24Format = hour12Format;
+            }else{
+                hour24Format = parseInt(hour12Format) +12;
+
+            }
+        }
+
+        return hour24Format+':'+aMinute;
+    }
+
     const editTask = async (e)=>{ 
         e.preventDefault(); 
+        const taskSnapshot = {...task, datetime: dateTime.year+"/"+dateTime.month+"/"+dateTime.day+" "+HoursConverter12FormatTo24Format(dateTime.hours, dateTime.minutes, dateTime.am_pm)}
+        setTask(taskSnapshot);
         const response = await fetch("http://localhost:5000/editTheTask/"+idtask,{
             method: "PUT",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(task),
+            body: JSON.stringify(taskSnapshot),
         });
         const data = await response.json();
         console.log(data);
@@ -45,20 +86,32 @@ function TaskForm(props){
 
     
   
-
+    
     return (
      
         <>
         <form onSubmit={editTask}>
         <h1>Actualizar Tarea</h1>
-
                 <label htmlFor="name">Titulo</label>
                 <input type="text" name="name" onChange={handleTask}/><br></br>
                 <label htmlFor="description">Descripcion</label>
                 <input type="text" name="description" onChange={handleTask}/><br></br>
                 <label htmlFor="grade">Nota</label>
                 <input type="number" name="grade" min="0.00" max="5.00" step="0.01" onChange={handleTask}/><br></br>
-           
+                <h3>Fecha</h3>
+                <label htmlFor="day">Dia</label>
+                <input type="text" name="day" onChange={handleDate}/>
+                <label htmlFor="month">Mes</label>
+                <input type="text" name="month" onChange={handleDate}/>
+                <label htmlFor="year">Año</label>
+                <input type="text" name="year" onChange={handleDate}/>
+                <h3>Hora</h3>
+                <label htmlFor="hours">Horas</label>
+                <input type="hours" name="hours" onChange={handleDate}/>
+                <label htmlFor="minutes">Minutos</label>
+                <input type="minutes" name="minutes" onChange={handleDate}/>
+                <label htmlFor="am_pm">AM/PM</label>
+                <input type="text" name="am_pm" onChange={handleDate}/>
         
         <button type='submit'>Actualizar Tarea +</button>
         </form>
